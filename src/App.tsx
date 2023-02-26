90b70ed3aa822482b22cb96c0c73e1f10f66a813
Joel Lozano <lozano.joel.a@gmail.com>
2023-02-25 22:51:45 -0500

    Finished homepage layout and added photo

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,11 @@ function App() {
     return (
         <>
             <NavBar />
-            <div className='container'>
-                <Routes>
-                    <Route path='/' element={<Home />} />
-                    <Route path='/projects' element={<Projects />} />
-                    <Route path='/about' element={<About />} />
-                </Routes>
-            </div>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/projects' element={<Projects />} />
+                <Route path='/about' element={<About />} />
+            </Routes>
         </>
     );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,42 +2,47 @@ import './Home.css';
 
 export default function Home() {
     return (
-        <>
-            <h1>Hi, I'm Joel Lozano!</h1>
-            <div className='media-icons'>
+        <div className='container'>
+            <div className='contacts'>
+                <h1>Joel Lozano</h1>
+                <div className='media-icons'>
+                    <a
+                        href='https://github.com/joel-lozano'
+                        rel='noopener noreferrer'
+                        target='_blank'
+                    >
+                        <img src='/icons/Github.png' alt='Github' />
+                    </a>
+                    <a
+                        href='https://www.linkedin.com/in/joelalozano/'
+                        rel='noopener noreferrer'
+                        target='_blank'
+                    >
+                        <img src='/icons/LinkedIn.png' alt='LinkedIn' />
+                    </a>
+                    <a
+                        href='https://www.facebook.com/joel.lozano.5011516/'
+                        rel='noopener noreferrer'
+                        target='_blank'
+                    >
+                        <img src='/icons/Facebook.png' alt='Facebook' />
+                    </a>
+                    <a href='mailto:lozano.joel.a@gmail.com'>
+                        <img src='/icons/Gmail.png' alt='Gmail' /> 
+                    </a>
+                </div>
                 <a
-                    href='https://github.com/joel-lozano'
+                    className='resume-link'
+                    href='/Lozano_Resume.pdf'
                     rel='noopener noreferrer'
                     target='_blank'
                 >
-                    <img src='/Github.png' alt='Github' />
-                </a>
-                <a
-                    href='https://www.linkedin.com/in/joelalozano/'
-                    rel='noopener noreferrer'
-                    target='_blank'
-                >
-                    <img src='/LinkedIn.png' alt='LinkedIn' />
-                </a>
-                <a
-                    href='https://www.facebook.com/joel.lozano.5011516/'
-                    rel='noopener noreferrer'
-                    target='_blank'
-                >
-                    <img src='/Facebook.png' alt='Facebook' />
-                </a>
-                <a href='mailto:lozano.joel.a@gmail.com'>
-                    <img src='/Gmail.png' alt='Gmail' /> 
+                    View Resume
                 </a>
             </div>
-            <a
-                className='resume-link'
-                href='/Lozano_Resume.pdf'
-                rel='noopener noreferrer'
-                target='_blank'
-            >
-                View Resume
-            </a>
-        </>
+            <div className='profile-photo'>
+                <img src='/Profile_Photo.jpg' />
+            </div>
+        </div>
     );
 }
