8c84ee2ef00d0264fa017c02809fdf2f15a09963
Joel Lozano <lozano.joel.a@gmail.com>
2023-08-27 20:57:43 -0400

    Add projects

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -5,6 +5,13 @@ export default function Projects() {
     return (
         <ul className='projects-container'>
             <ProjectDisplay
+                name='Article Generator'
+                image='/webpages/article-generator.png'
+                link='https://jlz-article-generator.azurewebsites.net'
+                headline='OpenAI API-connected article generator with read-aloud'
+                tags={['OpenAI API', 'ChatGPT', 'Dall-E', 'Microsoft Speech Service']}
+            />
+            <ProjectDisplay
                 name='Message Board'
                 image='/webpages/message-board.png'
                 link='https://jlz-message-board.azurewebsites.net'
@@ -13,16 +20,32 @@ export default function Projects() {
                 features={[
                     'Create, delete, and update posts',
                     'Supports user authentication',
-                    'Currently adding profile pictures'
+                    'Currently adding customizable profile pictures'
                 ]}
             />
             <ProjectDisplay
-                name='Article Generator'
-                image='/webpages/article-generator.png'
-                link='https://jlz-article-generator.azurewebsites.net'
-                headline='OpenAI API-connected article generator with read-aloud functionality'
-                tags={['OpenAI API', 'ChatGPT', 'Dall-E', 'Microsoft Speech Service']}
-                features={[]}
+                name='To-Do List'
+                image='/webpages/todo-list.png'
+                link='https://joel-lozano.github.io/todo-list'
+                headline='First React app'
+                tags={['React.js', 'JavaScript', 'HTML', 'CSS']}
+                features={[
+                    'Sort tasks by priority'
+                ]}
+            />
+            <ProjectDisplay
+                name='Snake Game'
+                image='/webpages/snake.png'
+                link='https://joel-lozano.github.io/snake-game'
+                headline='First frontend web development project'
+                tags={['JavaScript', 'HTML', 'CSS']}
+            />
+            <ProjectDisplay
+                name='Web Scraper'
+                image='/icons/Github.png'
+                link='https://github.com/joel-lozano/web-scraper'
+                headline='Custom search automation and webpage scraping'
+                tags={['Python', 'Bing Web Search API', 'Regular Expressions']}
             />
         </ul>
     );
